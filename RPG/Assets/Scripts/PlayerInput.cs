01d32ca1494ce5dddf7e24d26f0b5d688f14e719
Aloys <aloys.oberthuer@gmail.com>
2019-11-24 23:09:08 +0100

    Master Architects Course 5b - Camera & Horizontal Input

--- RPG/Assets/Scripts/PlayerInput.cs
+++ RPG/Assets/Scripts/PlayerInput.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerInput : IPlayerInput
 {
     public float Vertical => Input.GetAxis("Vertical");
-
     public float Horizontal => Input.GetAxis("Horizontal");
+    
     public float MouseX => Input.GetAxis("Mouse X");
 }
