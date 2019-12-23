33ea3307dec7f4e36310d9da2a9f43fb629efcf9
Aloys <aloys.oberthuer@gmail.com>
2019-12-23 17:47:59 +0100

    Master Architects Course 9a - Custom Inspectors

--- RPG/Assets/Scripts/IMover.cs
+++ RPG/Assets/Scripts/IMover.cs
@@ -1,4 +1,22 @@
+using UnityEditor;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
 public interface IMover
 {
     void Tick();
+}
+
+[CustomEditor(typeof(Item))]
+public class ItemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Item item = (Item) target;
+        
+        EditorGUILayout.LabelField("Custom Item Editor");
+        GUILayout.Box(item.Icon.texture, GUILayout.Width(64), GUILayout.Height(64));
+        
+        base.OnInspectorGUI();
+    }
 }
