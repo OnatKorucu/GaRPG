177f9769cf828164403082ec2742c5e45f798cd3
Aloys <aloys.oberthuer@gmail.com>
2020-02-12 08:30:54 +0100

    Master Architects Course 15d - Teardown

--- RPG/Assets/TestsPlay/game_state_machine.cs
+++ RPG/Assets/TestsPlay/game_state_machine.cs
@@ -8,11 +8,15 @@ namespace state_machine
 {
     public class game_state_machine
     {
+        [TearDown]
+        public void tear_down()
+        {
+            GameObject.Destroy(GameObject.FindObjectOfType<GameStateMachine>());
+        }
+
         [UnityTest]
         public IEnumerator switches_to_loading_when_level_to_load_selected()
         {
-            GameObject.Destroy(GameObject.FindObjectOfType<GameStateMachine>());
-            
             yield return Helpers.LoadMenuScene();
             var gameStateMachine = GameObject.FindObjectOfType<GameStateMachine>();
 
@@ -30,8 +34,6 @@ namespace state_machine
         [UnityTest]
         public IEnumerator switches_to_play_when_level_to_load_completed()
         {
-            GameObject.Destroy(GameObject.FindObjectOfType<GameStateMachine>());
-            
             yield return Helpers.LoadMenuScene();
             var gameStateMachine = GameObject.FindObjectOfType<GameStateMachine>();
 
@@ -42,25 +44,23 @@ namespace state_machine
             yield return null;
 
             Assert.AreEqual(typeof(Load), gameStateMachine.CurrentStateType);
-            
+
             yield return new WaitUntil(() => gameStateMachine.CurrentStateType == typeof(Play));
             Assert.AreEqual(typeof(Play), gameStateMachine.CurrentStateType);
         }
 
         [UnityTest]
-
         public IEnumerator only_allows_one_instance_to_exist()
         {
             var stateMachineOne = new GameObject("State Machine One").AddComponent<GameStateMachine>();
             var stateMachineTwo = new GameObject("State Machine Two").AddComponent<GameStateMachine>();
             yield return null;
-            
+
             // Cannot use Assert.IsNull on second game state machine as we have no control over when the GameObject is removed,
             // but using == will use Unity's overload of the operator which will return also true for <null>.
             // See for example: https://answers.unity.com/questions/865405/nunit-notnull-assert-strangeness.html
             Assert.IsNotNull(stateMachineOne);
-            Assert.IsTrue(stateMachineTwo == null); 
-            
+            Assert.IsTrue(stateMachineTwo == null);
         }
     }
 }
